5e22731d285f61dd5ed320ea16cb910015ab1282
Ryoji Kitano Neto <t.ryoji.neto@bankly.com.br>
2024-03-26 20:40:30 -0300

    adding commandDispatcher to handle commands

--- SM-POST/Post.Common/Events/PostRemovedEvent.cs
+++ SM-POST/Post.Common/Events/PostRemovedEvent.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CQRS.Core.Events;
 
 namespace Post.Common.Events
 {
-    public class PostRemovedEvent
+    public class PostRemovedEvent : BaseEvent
     {
-        
+        public PostRemovedEvent() : base(nameof(PostRemovedEvent))
+        {
+        }
     }
 }
